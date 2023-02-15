3aea0f9bd1e298e29fa726a19e41b8234ac58bf4
teaguehall <teaguehall11@gmail.com>
2023-02-14 20:10:53 -0600

    misc

--- project_1/tests.sh
+++ project_1/tests.sh
@@ -11,18 +11,72 @@ echo " 5 - Have two clients connect, one subscribes, one published - confirm tha
 echo " 6 - Have three clients connect, two subscribe, one published - confirm that both subscriber gets message"
 echo " 7 - Have two clients connect, one subscriber, one published, and then subscriber unsubscribes - confirm that subscriber does not receive message since it has unsubscribed"
 echo ""
-echo -n "Please enter test number:"
+echo -n "Please enter test number: "
 read TEST_CASE
 echo "-----------------------------------------------------------------------------"
 
 case $TEST_CASE in
 
   1)
-    echo "test"
+    ./client.out 127.0.0.1
+
     ;;
 
   2)
-    echo "test"
+    ./server.out &
+    sleep 1
+ 
+    ./client.out 127.0.0.1 "Sports;Tom;ESPN;" 20 &
+    sleep 1
+
+    kill %1 # kill server
+    sleep 5
+
+    pkill -P $$ # cleanup - kill all child processes
+    
+    ;;
+
+  3)
+    ./server.out &
+    sleep 1
+ 
+    ./client.out 127.0.0.1 "Sports;Tom;ESPN;" 20 &
+    sleep 1
+
+    kill %2 # kill client
+    sleep 20
+
+    pkill -P $$ # cleanup - kill all child processes
+    
+    ;;
+
+  4)
+    ./server.out &
+    sleep 1
+ 
+    ./client.out 127.0.0.1 "Sports;Tom;ESPN;" 20 & # subscriber 1
+    ./client.out 127.0.0.1 "Sports;Tom;ESPN;" 20 & # subscriber 2
+    ./client.out 127.0.0.1 "Sports;Tom;ESPN;" 20 & # subscriber 3
+    ./client.out 127.0.0.1 "Sports;Tom;ESPN;" 20 & # subscriber 4
+    ./client.out 127.0.0.1 "Sports;Tom;ESPN;" 20 & # subscriber 5 !!!! one too many
+    sleep 1
+
+    pkill -P $$ # cleanup - kill all child processes
+    
+    ;;
+
+  5)
+    ./server.out &
+    sleep 1
+ 
+    ./client.out 127.0.0.1 "Sports;Tom;ESPN;" 20 & # subscriber
+    sleep 1
+
+    ./client.out 127.0.0.1 "Sports;Tom;ESPN;Hello this is Tom from ESPN!" & # publisher
+    sleep 1
+
+    pkill -P $$ # cleanup - kill all child processes
+    
     ;;
 
   *)
