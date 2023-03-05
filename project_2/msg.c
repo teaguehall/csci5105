2e41350fb62de657258ca2db6b136a7fa2e70cf8
teaguehall <teaguehall11@gmail.com>
2023-03-05 11:33:27 -0600

    protocol message module completed. not tested...

--- project_2/msg.c
+++ project_2/msg.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <arpa/inet.h>
 
-// Builds message header, always succeeds and returns 0
+// Builds message header. Always succeeds and returns 0
 int msg_Build_Header(char* out_msg, uint32_t type,  uint32_t size)
 {
     // write magic number
@@ -18,7 +18,7 @@ int msg_Build_Header(char* out_msg, uint32_t type,  uint32_t size)
     return 0;
 }
 
-// Parses out header info from raw message. Returns 0 on success, -1 on error
+// Parse message header. Returns 0 on success, -1 on error
 int msg_Parse_Header(char* in_msg, uint32_t* out_type, uint32_t* out_size)
 {
     uint32_t magic = ntohl(*((uint32_t*)in_msg));
@@ -36,7 +36,23 @@ int msg_Parse_Header(char* in_msg, uint32_t* out_type, uint32_t* out_size)
     return 0;
 }
 
-// Parses out ERROR response from raw message
+// Builds MSG_TYPE_ERROR_RESPONSE message. Returns 0 on success, -1 on error
+int msg_Build_ErrorResponse(char* out_msg, char* in_err_string)
+{
+    uint32_t bytes_written = 0;
+    
+    // copy error string to message
+    strcpy(out_msg + MSG_HEADER_OFFSET, in_err_string);
+    bytes_written += strlen(in_err_string) + 1;
+
+    // write message header
+    msg_Build_Header(out_msg, MSG_TYPE_ERROR_RESPONSE,  bytes_written);
+
+    // success
+    return 0;
+}
+
+// Parses MSG_TYPE_ERROR_RESPONSE message. Returns 0 on success, -1 on error
 int msg_Parse_ErrorResponse(char* in_msg, char* out_error_msg)
 {
     uint32_t msg_type, msg_size;
@@ -44,14 +60,14 @@ int msg_Parse_ErrorResponse(char* in_msg, char* out_error_msg)
     // validate header
     if(msg_Parse_Header(in_msg, &msg_type, &msg_size))
     {
-        fprintf(stderr, "ERROR: Invalid header found while parsing Error Response message\n");
+        fprintf(stderr, "ERROR: Invalid header found while parsing MSG_TYPE_ERROR_RESPONSE message\n");
         return -1;
     }
     
     // validate type
     if(msg_type != MSG_TYPE_ERROR_RESPONSE)
     {
-        fprintf(stderr, "ERROR: Incorrect message type while parsing Error Response message. Found %d, Expected %d\n", msg_type, MSG_TYPE_ERROR_RESPONSE);
+        fprintf(stderr, "ERROR: Incorrect message type while parsing MSG_TYPE_ERROR_RESPONSE message. Found %d, Expected %d\n", msg_type, MSG_TYPE_ERROR_RESPONSE);
         return -1;
     }
 
@@ -62,7 +78,7 @@ int msg_Parse_ErrorResponse(char* in_msg, char* out_error_msg)
     return 0;
 }
 
-// Builds POST request to raw message
+// Builds MSG_TYPE_POST_REQUEST message. Returns 0 on success, -1 on error
 int msg_Build_PostRequest(char* out_msg, char* in_author, char* in_title, char* in_contents)
 {
     uint32_t bytes_written = 0;
@@ -86,7 +102,7 @@ int msg_Build_PostRequest(char* out_msg, char* in_author, char* in_title, char* 
     return 0;
 }
 
-// Parses out POST REQUEST info from raw message
+// Parses MSG_TYPE_POST_REQUEST message. Returns 0 on success, -1 on error
 int msg_Parse_PostRequest(char* in_msg, char* out_author, char* out_title, char* out_contents)
 {
     uint32_t msg_type, msg_size;
@@ -95,14 +111,14 @@ int msg_Parse_PostRequest(char* in_msg, char* out_author, char* out_title, char*
     // validate header
     if(msg_Parse_Header(in_msg, &msg_type, &msg_size))
     {
-        fprintf(stderr, "ERROR: Invalid header found while parsing POST REQUEST message\n");
+        fprintf(stderr, "ERROR: Invalid header found while parsing MSG_TYPE_POST_REQUEST message\n");
         return -1;
     }
     
     // validate type
     if(msg_type != MSG_TYPE_POST_REQUEST)
     {
-        fprintf(stderr, "ERROR: Incorrect message type while parsing POST REQUEST message. Found %d, Expected %d\n", msg_type, MSG_TYPE_POST_REQUEST);
+        fprintf(stderr, "ERROR: Incorrect message type while parsing MSG_TYPE_POST_REQUEST message. Found %d, Expected %d\n", msg_type, MSG_TYPE_POST_REQUEST);
         return -1;
     }
 
@@ -122,14 +138,7 @@ int msg_Parse_PostRequest(char* in_msg, char* out_author, char* out_title, char*
     return 0;
 }
 
-    // message structure:
-    // - magic number (uint32 4-bytes)
-    // - message type (uint32 4-bytes)
-    // - message size (uint32 4-bytes)
-    // - page size (uint32 4-bytes)
-    // - page number (uint32 4-bytes)
-
-// Builds read request message
+// Builds MSG_TYPE_READ_REQUEST message. Returns 0 on success, -1 on error
 int msg_Build_ReadRequest(char* out_msg, uint32_t page_size, uint32_t page_number)
 {
     uint32_t bytes_written = 0;
@@ -149,7 +158,7 @@ int msg_Build_ReadRequest(char* out_msg, uint32_t page_size, uint32_t page_numbe
     return 0;    
 }
 
-// Parses out READ REQUEST info from raw message 
+// Parses MSG_TYPE_READ_REQUEST message. Returns 0 on success, -1 on error
 int msg_Parse_ReadRequest(char* in_msg, uint32_t* out_page_size, uint32_t* out_page_number)
 {
     uint32_t msg_type, msg_size;
@@ -181,7 +190,7 @@ int msg_Parse_ReadRequest(char* in_msg, uint32_t* out_page_size, uint32_t* out_p
     return 0;
 }
 
-// Builds MSG_TYPE_READ_RESPONSE message
+// Builds MSG_TYPE_READ_RESPONSE message. Returns 0 on success, -1 on error
 int msg_Build_ReadResponse(char* out_msg, int article_count, Article articles[])
 {
     uint32_t bytes_written = 0;
@@ -222,114 +231,233 @@ int msg_Build_ReadResponse(char* out_msg, int article_count, Article articles[])
     return 0;   
 }
 
-
-    // message structure:
-    // - magic number (uint32 4-bytes)
-    // - message type (uint32 4-bytes)
-    // - message size (uint32 4-bytes)
-    // - number of articles (uint32 4-bytes)
-    //      - article ID (uint32 4-bytes)
-    //      - parent article ID (uint32 4-bytes)
-    //      - article author (null-terminated string)
-    //      - article title (null-terminated string)
-    //      - article contents (null-terminated string)
-
-
-// Parses out READ response info from raw message
-int msg_Parse_ReadResponse(char* in_msg, uint32_t* out_article_count, Article* out_articles)
+// Parses MSG_TYPE_READ_RESPONSE message. Returns 0 on success, -1 on error
+int msg_Parse_ReadResponse(char* in_msg, uint32_t* out_article_count, Article out_articles[])
 {
-    // TODO
+    uint32_t msg_type, msg_size;
+    uint32_t bytes_read = 0;
 
-    // success
-    return 0;
-}
-
-// Parses out CHOOSE request info from raw message
-int msg_Parse_ChooseRequest(char* in_msg, uint32_t* out_article_id)
-{
-    // TODO
-
-    // success
-    return 0;
-}
-
-// Parses out CHOOSE response info from raw message
-int msg_Parse_ChooseResponse(char* in_msg, Article* out_articles)
-{
-    // TODO
-
-    // success
-    return 0;
-}
-
-// Parses out REPLAY request from raw message
-int msg_Parse_ReplyRequest(char* in_msg, uint32_t* out_article_id, char* out_author, char* out_contents)
-{
-    // TODO
-
-    // success
-    return 0;
-}
-
-
-
-// Builds ERROR response message to raw output message
-int msg_Build_ErrorResponse(char* out_msg, char* in_err_string)
-{
-    uint32_t bytes_written = 0;
+    // validate header
+    if(msg_Parse_Header(in_msg, &msg_type, &msg_size))
+    {
+        fprintf(stderr, "ERROR: Invalid header found while parsing MSG_TYPE_READ_RESPONSE message\n");
+        return -1;
+    }
     
-    // copy error string to message
-    strcpy(out_msg + MSG_HEADER_OFFSET, in_err_string);
-    bytes_written += strlen(in_err_string) + 1;
+    // validate type
+    if(msg_type != MSG_TYPE_READ_RESPONSE)
+    {
+        fprintf(stderr, "ERROR: Incorrect message type while parsing MSG_TYPE_READ_RESPONSE message. Found %d, Expected %d\n", msg_type, MSG_TYPE_READ_RESPONSE);
+        return -1;
+    }
 
-    // write message header
-    msg_Build_Header(out_msg, MSG_TYPE_ERROR_RESPONSE,  bytes_written);
+    // parse out article count
+    *out_article_count = ntohl(*(uint32_t*)(in_msg + MSG_HEADER_OFFSET + bytes_read));
+    bytes_read += 4;
+
+    // parse out each article
+    for(int i = 0; i < *out_article_count; i++)
+    {
+        // parse out article ID
+        out_articles[i].id = ntohl(*(uint32_t*)(in_msg + MSG_HEADER_OFFSET + bytes_read));
+        bytes_read += 4;
+
+        // parse out parent article ID
+        out_articles[i].parent_id = ntohl(*(uint32_t*)(in_msg + MSG_HEADER_OFFSET + bytes_read));
+        bytes_read += 4;
+
+        // parse out article author
+        strcpy(out_articles[i].author, in_msg + MSG_HEADER_OFFSET + bytes_read);
+        bytes_read += strlen(out_articles[i].author) + 1;
+
+        // parse out article title
+        strcpy(out_articles[i].title, in_msg + MSG_HEADER_OFFSET + bytes_read);
+        bytes_read += strlen(out_articles[i].title) + 1;
+
+        // parse out article contents
+        strcpy(out_articles[i].contents, in_msg + MSG_HEADER_OFFSET + bytes_read);
+        bytes_read += strlen(out_articles[i].contents) + 1;
+    }
 
     // success
     return 0;
 }
 
-
-
-int msg_Build_PostResponse(char* out_msg)
-{
-    // TODO
-
-    // success
-    return 0;
-}
-
-
-
-
-
+// Builds MSG_TYPE_CHOOSE_REQUEST message. Returns 0 on success, -1 on error
 int msg_Build_ChooseRequest(char* out_msg, uint32_t article_id)
 {
-    // TODO
+    uint32_t bytes_written = 0;
 
+    // write article count
+    *(uint32_t*)(out_msg + MSG_HEADER_OFFSET + bytes_written) = htonl(article_id);
+    bytes_written += 4;
+
+    // write header
+    msg_Build_Header(out_msg, MSG_TYPE_CHOOSE_REQUEST,  bytes_written);
+    
     // success
-    return 0;
+    return 0;   
 }
 
-int msg_Build_ChooseResponse(char* out_msg, Article articles)
+// Parses MSG_TYPE_CHOOSE_REQUEST message. Returns 0 on success, -1 on error
+int msg_Parse_ChooseRequest(char* in_msg, uint32_t* out_article_id)
 {
-    // TODO
+    uint32_t msg_type, msg_size;
+    uint32_t bytes_read = 0;
+
+    // validate header
+    if(msg_Parse_Header(in_msg, &msg_type, &msg_size))
+    {
+        fprintf(stderr, "ERROR: Invalid header found while parsing MSG_TYPE_CHOOSE_REQUEST message\n");
+        return -1;
+    }
+    
+    // validate type
+    if(msg_type != MSG_TYPE_CHOOSE_REQUEST)
+    {
+        fprintf(stderr, "ERROR: Incorrect message type while parsing MSG_TYPE_CHOOSE_REQUEST message. Found %d, Expected %d\n", msg_type, MSG_TYPE_CHOOSE_REQUEST);
+        return -1;
+    }
+
+    // parse out article count
+    *out_article_id = ntohl(*(uint32_t*)(in_msg + MSG_HEADER_OFFSET + bytes_read));
+    bytes_read += 4;
 
     // success
     return 0;
 }
 
+// Builds MSG_TYPE_CHOOSE_RESPONSE message. Returns 0 on success, -1 on error
+int msg_Build_ChooseResponse(char* out_msg, Article article)
+{
+    uint32_t bytes_written = 0;
+
+    // write article ID
+    *(uint32_t*)(out_msg + MSG_HEADER_OFFSET + bytes_written) = htonl(article.id);
+    bytes_written += 4;
+
+    // write parent article ID
+    *(uint32_t*)(out_msg + MSG_HEADER_OFFSET + bytes_written) = htonl(article.parent_id);
+    bytes_written += 4;
+
+    // write article author
+    strcpy(out_msg + MSG_HEADER_OFFSET + bytes_written, article.author);
+    bytes_written += strlen(article.author) + 1;
+
+    // write article title
+    strcpy(out_msg + MSG_HEADER_OFFSET + bytes_written, article.title);
+    bytes_written += strlen(article.title) + 1;
+
+    // write article contents
+    strcpy(out_msg + MSG_HEADER_OFFSET + bytes_written, article.contents);
+    bytes_written += strlen(article.contents) + 1;
+
+    // write header
+    msg_Build_Header(out_msg, MSG_TYPE_CHOOSE_RESPONSE,  bytes_written);
+    
+    // success
+    return 0;   
+}
+
+// Parses MSG_TYPE_CHOOSE_RESPONSE message. Returns 0 on success, -1 on error
+int msg_Parse_ChooseResponse(char* in_msg, Article* out_article)
+{
+    uint32_t msg_type, msg_size;
+    uint32_t bytes_read = 0;
+
+    // validate header
+    if(msg_Parse_Header(in_msg, &msg_type, &msg_size))
+    {
+        fprintf(stderr, "ERROR: Invalid header found while parsing MSG_TYPE_CHOOSE_RESPONSE message\n");
+        return -1;
+    }
+    
+    // validate type
+    if(msg_type != MSG_TYPE_CHOOSE_RESPONSE)
+    {
+        fprintf(stderr, "ERROR: Incorrect message type while parsing MSG_TYPE_CHOOSE_RESPONSE message. Found %d, Expected %d\n", msg_type, MSG_TYPE_CHOOSE_RESPONSE);
+        return -1;
+    }
+
+    // parse out article ID
+    out_article->id = ntohl(*(uint32_t*)(in_msg + MSG_HEADER_OFFSET + bytes_read));
+    bytes_read += 4;  
+
+    // parse out parent article ID
+    out_article->parent_id = ntohl(*(uint32_t*)(in_msg + MSG_HEADER_OFFSET + bytes_read));
+    bytes_read += 4;    
+
+    // parse out article author
+    strcpy(out_article->author, in_msg + MSG_HEADER_OFFSET + bytes_read);
+    bytes_read += strlen(out_article->author) + 1;   
+
+    // parse out article title
+    strcpy(out_article->title, in_msg + MSG_HEADER_OFFSET + bytes_read);
+    bytes_read += strlen(out_article->title) + 1;   
+
+    // parse out article contents
+    strcpy(out_article->contents, in_msg + MSG_HEADER_OFFSET + bytes_read);
+    bytes_read += strlen(out_article->contents) + 1;
+
+    // success
+    return 0;
+}
+
+// Builds MSG_TYPE_REPLY_REQUEST message. Returns 0 on success, -1 on error
 int msg_Build_ReplyRequest(char* out_msg, uint32_t article_id, char* author, char* contents)
 {
-    // TODO
+    uint32_t bytes_written = 0;
 
+    // write article ID to which is being replied
+    *(uint32_t*)(out_msg + MSG_HEADER_OFFSET + bytes_written) = htonl(article_id);
+    bytes_written += 4;
+
+    // write author of replier
+    strcpy(out_msg + MSG_HEADER_OFFSET + bytes_written, author);
+    bytes_written += strlen(author) + 1;
+
+    // write contents of reply
+    strcpy(out_msg + MSG_HEADER_OFFSET + bytes_written, contents);
+    bytes_written += strlen(contents) + 1;
+
+    // write header
+    msg_Build_Header(out_msg, MSG_TYPE_REPLY_REQUEST,  bytes_written);
+    
     // success
     return 0;
 }
 
-int msg_Build_ReplyResponse(char* out_msg)
+// Parses MSG_TYPE_REPLY_REQUEST message. Returns 0 on success, -1 on error
+int msg_Parse_ReplyRequest(char* in_msg, uint32_t* out_article_id, char* out_author, char* out_contents)
 {
-    // TODO
+    uint32_t msg_type, msg_size;
+    uint32_t bytes_read = 0;
+
+    // validate header
+    if(msg_Parse_Header(in_msg, &msg_type, &msg_size))
+    {
+        fprintf(stderr, "ERROR: Invalid header found while parsing MSG_TYPE_REPLY_REQUEST message\n");
+        return -1;
+    }
+    
+    // validate type
+    if(msg_type != MSG_TYPE_REPLY_REQUEST)
+    {
+        fprintf(stderr, "ERROR: Incorrect message type while parsing MSG_TYPE_REPLY_REQUEST message. Found %d, Expected %d\n", msg_type, MSG_TYPE_REPLY_REQUEST);
+        return -1;
+    }
+
+    // parse out article ID
+    *out_article_id = ntohl(*(uint32_t*)(in_msg + MSG_HEADER_OFFSET + bytes_read));
+    bytes_read += 4;  
+
+    // parse out author
+    strcpy(out_author, in_msg + MSG_HEADER_OFFSET + bytes_read);
+    bytes_read += strlen(out_author) + 1;
+
+    // parse out contents
+    strcpy(out_contents, in_msg + MSG_HEADER_OFFSET + bytes_read);
+    bytes_read += strlen(out_contents) + 1; 
 
     // success
     return 0;
