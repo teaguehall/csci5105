04c00a0aa27408c2b86311cf31a45e4b55baff31
teaguehall <teaguehall11@gmail.com>
2023-03-09 20:01:11 -0600

    Added dummy server code for network testing

--- project_2/server/server.c
+++ project_2/server/server.c
@@ -1,10 +1,83 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "../shared/tcp.h"
+#include "../shared/msg.h"
+#include "../shared/article.h"
+
 int main(int argc, char * argv[])
 {
-    printf("Hello from server!\n");
+    int listener_socket = -1;
+    char local_addr[] = "127.0.0.1";
+    int local_port = 555;
+    
+    int remote_socket = -1;
+    char remote_addr[64];
+    int remote_port;
 
+    char recv_msg[4096];
+    char send_msg[4096];
+
+    uint32_t msg_recv_type;
+    uint32_t msg_recv_size;
+    
+    // create listener socket
+    if(tcp_CreateListener(local_addr, local_port, &listener_socket))
+    {
+        printf("Failed to create listener socket\n");
+        return -1;
+    }
+    
+    // accept connections
+    while(1)
+    {
+        // accept connection
+        if(tcp_Accept(listener_socket, remote_addr, &remote_port, &remote_socket))
+        {
+            printf("ERROR occurred while accepting connection\n");
+            return -1;
+        }
+
+        printf("Accepted connection from client %s:%d\n", remote_addr, remote_port);
+
+        // wait for header
+        if(tcp_Recv(listener_socket, recv_msg, MSG_HEADER_OFFSET, 5))
+        {
+            printf("ERROR occurred while accepting connection\n");
+            return -1;
+        }
+
+        // extract message header info
+        if(msg_Parse_Header(recv_msg, &msg_recv_type, &msg_recv_size))
+        {
+            printf("Server received invalid header from client\n");
+            return -1;
+        }
+
+        // read rest of message
+        if(tcp_Recv(listener_socket, recv_msg + MSG_HEADER_OFFSET, msg_recv_size, 5))
+        {
+            printf("ERROR occurred while accepting connection\n");
+            return -1;
+        }
+
+        // extract post message
+        char author[4096];
+        char title[4096];
+        char contents[4096];
+
+        if(msg_Parse_PostRequest(recv_msg, author, title, contents))
+        {
+            printf("ERROR while parsing Post request message");
+            return -1;
+        }
+
+        // print debug message
+        printf("Server received post: author = %s, title = %s, contents = %s\n", author, title, contents);
+
+        // disconnect from client
+        tcp_Disconnect(listener_socket);
+    }
 
     return EXIT_SUCCESS;
 }
