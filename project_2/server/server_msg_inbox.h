ea3951e039962e6bad77f49952ff81ac007b3b69
teaguehall <teaguehall11@gmail.com>
2023-03-25 23:18:11 -0500

    mic

--- project_2/server/server_msg_inbox.h
+++
@@ -1,7 +0,0 @@
-#ifndef SERVER_MSG_OUTBOX_H
-#define SERVER_MSG_OUTBOX_H
-
-int serverMsgInbox_Insert(char* msg_to_insert);
-int serverMsgInbox_WaitFor(int timeout_sec, int msg_id, char* out_msg);
-
-#endif // SERVER_MSG_OUTBOX_H
